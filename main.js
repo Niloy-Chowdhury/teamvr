57cfa9df246fd2f7d8f8d84a5317663b7b16f5c0
Niloy-Chowdhury <82151867+Niloy-Chowdhury@users.noreply.github.com>
2021-04-16 19:32:30 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//Curtain Loading Page
+let firstPage = document.querySelector(".firstPage");
+let secondPage = document.querySelector(".secondPage");
+window.onload = function(){
+    firstPage.style.top = '-100%';
+    secondPage.style.bottom = '-100%';
+}
+//Rounded Loading Page
+// window.onload = function(){
+//     firstPage.style.width = '0px';
+//     firstPage.style.height = '0px';
+// }
